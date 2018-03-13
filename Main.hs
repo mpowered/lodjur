fb4f0430659d7598a06a0d29834b057fbb59f924
Oskar Wickström <oskar.wickstrom@gmail.com>
2018-03-13 15:36:53 +0100

    Use MVar for deployment state

--- Main.hs
+++ Main.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase        #-}
 
 import Data.Semigroup
 import Web.Scotty.Trans
+import Control.Concurrent
 import Control.Monad
 import Data.Text (Text)
 import qualified Data.Text.Lazy as Lazy
@@ -16,7 +18,9 @@ import Lucid.Html5
 renderHtml :: Html () -> Action ()
 renderHtml = html . Html.renderText
 
-type LodjurEnv = ()
+data LodjurState = Idle | Deploying
+
+type LodjurEnv = MVar LodjurState
 
 type Scotty = ScottyT Lazy.Text (ReaderT LodjurEnv IO)
 type Action = ActionT Lazy.Text (ReaderT LodjurEnv IO)
@@ -36,13 +40,29 @@ showAllTags = do
         option_ [value_ tag] (toHtml tag)
       input_ [type_ "submit", value_ "Deploy"]
 
+modifyState :: (LodjurState -> Action LodjurState) -> Action ()
+modifyState f = do
+  var <- lift ask
+  state <- liftIO (takeMVar var)
+  liftIO . putMVar var =<< f state
+
 deployTag :: Action ()
 deployTag = do
   tag <- param "tag"
-  renderHtml $ do
-    p_ $ toHtml $ "Deploying tag " <> (tag :: Tag)
+  modifyState $
+    \case
+      Idle -> do
+        renderHtml $ do
+          p_ $ toHtml $ "Deploying tag " <> (tag :: Tag)
+        return Deploying
+      Deploying -> do
+        renderHtml $ do
+          p_ "Already deploying a tag."
+        return Idle
 
 main :: IO ()
-main = scottyT 4000 (flip runReaderT ()) $ do
-  get "/" showAllTags
-  post "/" deployTag
+main = do
+  mvar <- newMVar Idle
+  scottyT 4000 (flip runReaderT mvar) $ do
+    get "/" showAllTags
+    post "/" deployTag
