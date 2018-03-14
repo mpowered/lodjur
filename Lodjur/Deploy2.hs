51a72a436ec9974a96c00db5fa62bcb322222f10
Oskar Wickström <oskar.wickstrom@gmail.com>
2018-03-14 23:19:43 +0100

    Refine state and messages

--- Lodjur/Deploy2.hs
+++ Lodjur/Deploy2.hs
@@ -14,6 +14,7 @@ import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HashMap
 import Data.String
 import Data.Text (Text)
+import qualified Data.Text.IO as Text
 
 import Lodjur.Actor
 
@@ -21,11 +22,11 @@ newtype Tag = Tag { unTag :: Text } deriving (Eq, Show, IsString)
 
 newtype DeploymentName = DeploymentName String deriving (Eq, Show, IsString)
 
+type DeployId = Text
+
 data DeployState
   = Idle
-  | Deploying
-
-type DeployId = Text
+  | Deploying Tag DeployId
 
 newtype DeployResult = DeployResult Text deriving (Show, Eq)
 
@@ -47,8 +48,11 @@ deploy di = do
   return (DeployResult (di <> " is done!"))
 
 data DeployMessage r where
+  -- Public messages:
   Deploy :: Tag -> DeployMessage (Sync (Maybe DeployId))
-  GetDeployStatus :: DeployId -> DeployMessage (Sync (Maybe DeployResult))
+  GetResult :: DeployId -> DeployMessage (Sync (Maybe DeployResult))
+  GetCurrentState :: DeployMessage (Sync DeployState)
+  -- Private messages:
   DeployFinished :: DeployId -> DeployResult -> DeployMessage Async
 
 instance Actor DeployActor where
@@ -56,24 +60,30 @@ instance Actor DeployActor where
 
   receive self (a@DeployActor{state, history}, msg)=
     case (state, msg) of
-      (Idle     , Deploy _) -> do
-        putStrLn "Deploying ..."
+      (Idle     , Deploy tag) -> do
         let di = "deploy-1"
+        Text.putStrLn ("Deploying tag " <> unTag tag <> ". Deploy ID: " <> di)
         void $ forkIO $ do
           result <- deploy di
           self ! DeployFinished di result
-        return (a { state = Deploying }, Just di)
+        return (a { state = Deploying tag di}, Just di)
       (Idle     , DeployFinished _ _) ->
         return a { state = Idle }
-      (Deploying, Deploy _        ) -> do
-        putStrLn "Ignoring."
-        return (a { state = Deploying }, Nothing)
-      (Deploying, DeployFinished di result) -> do
-        putStrLn "Deploy finished!"
-        return a { state = Idle, history = HashMap.insert di result history }
-      (_, GetDeployStatus di) ->
+      (Deploying{}, Deploy _        ) -> do
+        Text.putStrLn "Ignoring."
+        return (a, Nothing)
+      (Deploying _ currentId, DeployFinished di result)
+        | currentId == di -> do
+          Text.putStrLn ("Deploy finished: " <> di)
+          return a { state = Idle, history = HashMap.insert di result history }
+        | otherwise -> do
+          Text.putStrLn ("Cannot mark deploy as finished: " <> di)
+          return a
+      (_, GetCurrentState) ->
+        return (a, state)
+      (_, GetResult di) ->
         return (a { state = Idle }, HashMap.lookup di history)
 
   shutdown DeployActor {state} = case state of
     Idle -> return ()
-    Deploying -> putStrLn "Killed while deploying"
+    Deploying tag _ -> putStrLn ("Killed while deploying " <> show tag)
