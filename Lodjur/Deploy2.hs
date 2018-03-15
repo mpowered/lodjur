ec1829c6a9172f1ccc4e9d4a5f39ffab278679e5
Oskar Wickström <oskar.wickstrom@gmail.com>
2018-03-15 10:00:09 +0100

    Implement deploy and list tags as before

--- Lodjur/Deploy2.hs
+++ Lodjur/Deploy2.hs
@@ -18,7 +18,7 @@ module Lodjur.Deploy2
   , initialize
   ) where
 
-import Control.Exception (SomeException)
+import Control.Exception (Exception, SomeException, throwIO)
 import Data.Semigroup
 import Control.Monad (void)
 import Control.Concurrent
@@ -31,13 +31,17 @@ import Data.HashSet (HashSet)
 import qualified Data.HashSet as HashSet
 import GHC.Generics (Generic)
 import Data.Hashable (Hashable)
+import System.Exit
+import System.Process (proc, readCreateProcessWithExitCode, CreateProcess (cwd))
 
 import Lodjur.Process
 
-newtype Tag = Tag { unTag :: Text } deriving (Eq, Show, IsString)
+newtype Tag =
+  Tag { unTag :: Text }
+  deriving (Eq, Show, IsString)
 
 newtype DeploymentName =
-  DeploymentJobName String
+  DeploymentName { unDeploymentName :: String }
   deriving (Eq, Show, IsString, Generic, Hashable)
 
 type JobId = Text
@@ -71,6 +75,7 @@ data DeployMessage r where
   Deploy :: DeploymentName -> Tag -> DeployMessage (Sync (Maybe DeploymentJob))
   GetResult :: JobId -> DeployMessage (Sync (Maybe DeployResult))
   GetCurrentState :: DeployMessage (Sync DeployState)
+  GetTags :: DeployMessage (Sync [Tag])
   -- Private messages:
   DeployFinished :: DeploymentJob -> DeployResult -> DeployMessage Async
 
@@ -78,10 +83,43 @@ initialize :: HashSet DeploymentName -> FilePath -> DeployActor
 initialize deploymentNames gitWorkingDir =
   DeployActor {state = Idle, history = mempty, ..}
 
-deploy :: DeploymentJob -> IO DeployResult
-deploy d = do
-  threadDelay $ 5 * 1000 * 1000
-  return (DeploySuccessful d)
+data GitFailed = GitFailed String String Int
+  deriving (Eq, Show)
+
+instance Exception GitFailed
+
+data NixopsFailed = NixopsFailed String String Int
+  deriving (Eq, Show)
+
+instance Exception NixopsFailed
+
+gitCmd :: [String] -> FilePath -> IO String
+gitCmd args gitWorkingDir = do
+  (exitcode, stdout, stderr) <- readCreateProcessWithExitCode
+    ((proc "git" args) { cwd = Just gitWorkingDir })
+    ""
+  case exitcode of
+    ExitSuccess      -> return stdout
+    ExitFailure code -> throwIO (GitFailed stdout stderr code)
+
+nixopsCmd :: [String] -> IO String
+nixopsCmd args = do
+  (exitcode, stdout, stderr) <- readCreateProcessWithExitCode
+    (proc "nixops" args)
+    ""
+  case exitcode of
+    ExitSuccess      -> return stdout
+    ExitFailure code -> throwIO (NixopsFailed stdout stderr code)
+
+deploy :: FilePath -> DeploymentJob -> IO DeployResult
+deploy gitWorkingDir job = do
+  _ <- gitCmd ["checkout", Text.unpack (jobId job)] gitWorkingDir
+  _ <- nixopsCmd ["deploy", "-d", unDeploymentName (deploymentName job)]
+  return (DeploySuccessful job)
+
+gitListTags :: FilePath -> IO [Tag]
+gitListTags workingDir = parseTags <$> gitCmd ["tag", "-l"] workingDir
+  where parseTags = map Tag . filter (not . Text.null) . Text.lines . Text.pack
 
 notifyDeployFinished
   :: Ref DeployActor
@@ -95,14 +133,14 @@ notifyDeployFinished self d r = do
 instance Process DeployActor where
   type Message DeployActor = DeployMessage
 
-  receive self (a@DeployActor{state, history, deploymentNames}, msg)=
+  receive self (a@DeployActor{..}, msg)=
     case (state, msg) of
       (Idle     , Deploy name tag)
         -- We require the deployment name to be known.
         | HashSet.member name deploymentNames -> do
-          let d = DeploymentJob { deploymentTag = tag, jobId = "deploy-1", deploymentName = name }
-          void (forkFinally (deploy d) (notifyDeployFinished self d))
-          return (a { state = Deploying d}, Just d)
+          let job = DeploymentJob { deploymentTag = tag, jobId = "deploy-1", deploymentName = name }
+          void (forkFinally (deploy gitWorkingDir job) (notifyDeployFinished self job))
+          return (a { state = Deploying job}, Just job)
         -- We can't deploy to an unknown deployment.
         | otherwise -> return (a, Nothing)
       (Idle     , DeployFinished _ _) ->
@@ -116,6 +154,9 @@ instance Process DeployActor where
         | otherwise -> do
           putStrLn ("Cannot mark deploy job as finished: " <> show job)
           return a
+      (_, GetTags) -> do
+        tags <- gitListTags gitWorkingDir
+        return (a, tags)
       (_, GetCurrentState) ->
         return (a, state)
       (_, GetResult di) ->
