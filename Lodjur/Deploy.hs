e3198e1f51113a54652c1c76c099d28cf197faaa
Shaun Sharples <shaun.sharples@gmail.com>
2018-03-15 13:10:46 +0200

    git checkout recurse submodules

--- Lodjur/Deploy.hs
+++ Lodjur/Deploy.hs
@@ -121,7 +121,7 @@ nixopsCmd args = do
 
 deploy :: FilePath -> DeploymentJob -> IO ()
 deploy gitWorkingDir job = do
-  _ <- gitCmd ["checkout", Text.unpack (jobId job)] gitWorkingDir
+  _ <- gitCmd ["checkout", Text.unpack (jobId job), "--recurse-submodules"] gitWorkingDir
   _ <- nixopsCmd ["deploy", "-d", unDeploymentName (deploymentName job)]
   return ()
 
