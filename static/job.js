f09d23eec465be620c08b5f73e84bad4859f1986
Oskar Wickström <oskar.wickstrom@gmail.com>
2018-03-21 14:08:12 +0100

    Print timestamps in streaming

--- static/job.js
+++ static/job.js
@@ -1,3 +1,23 @@
+/* Whether `t1` is before `t2`, with a resolution of whole seconds.
+ */
+function earlierThanInSeconds(t1, t2) {
+  console.log(t1, t2);
+  return Math.floor(t1.valueOf() / 1000) <  Math.floor(t2.valueOf() / 1000);
+}
+
+function renderTimeStamp(t) {
+  function padZero(n) {
+    return (n < 10) ? '0' + n : '' + n;
+  }
+  var timestamp = document.createElement('time');
+  timestamp.setAttribute('datetime', t.toISOString());
+  timestamp.innerText =
+    padZero(t.getUTCHours()) + ':' +
+    padZero(t.getUTCMinutes()) + ':' +
+    padZero(t.getUTCSeconds());
+  return timestamp;
+}
+
 function subscribeToOutput(outputContainer) {
   var preElement = outputContainer.firstElementChild;
   var jobId = outputContainer.dataset.jobId;
@@ -8,15 +28,25 @@ function subscribeToOutput(outputContainer) {
     outputContainer.classList.add('streaming');
 
     var stream = new EventSource('/jobs/' + jobId + '/output?from=' + lastLineAt.toISOString());
+    var lastTimestamp = lastLineAt;
 
     stream.addEventListener('output', function (e) {
       var event = JSON.parse(e.data);
-      console.log('Got stuff sent at', event.outputEventTime);
+      var eventTimestamp = new Date(event.outputEventTime);
 
       event.outputEventLines.forEach(function (outputLine) {
         var line = document.createElement('div');
         line.classList.add('line');
+
+        // Add timestamp if not same second as before.
+        if (true || earlierThanInSeconds(lastTimestamp, eventTimestamp)) {
+          line.append(renderTimeStamp(eventTimestamp));
+        }
+        lastTimestamp = eventTimestamp;
+
+        // Add the output content.
         line.append(outputLine);
+
         preElement.appendChild(line);
       });
     });
@@ -29,7 +59,7 @@ function subscribeToOutput(outputContainer) {
 
     stream.onerror = function (e) {
       outputContainer.classList.remove('streaming');
-      console.error(e);
+      console.error(e.message, e);
       stream.close();
     };
   }
