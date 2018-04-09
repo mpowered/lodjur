9679ce0b6430515eb87063cc04612b5e58b9cfd2
Shaun Sharples <shaun.sharples@gmail.com>
2018-04-09 12:14:08 +0200

    update nix files, bumped to version 0.1.0

--- default.nix
+++ default.nix
@@ -1,21 +1,22 @@
 { mkDerivation, aeson, base, base16-bytestring, binary, bytestring
-, cryptonite, hashable, http-types, iso8601-time, lucid
+, cryptonite, hashable, htoml, http-types, iso8601-time, lucid
 , monad-control, mtl, optparse-applicative, postgresql-simple
 , process, resource-pool, scotty, stdenv, text, time
 , unordered-containers, uuid, wai, wai-extra, wai-middleware-static
 }:
 mkDerivation {
   pname = "lodjur";
-  version = "0.0.0";
+  version = "0.1.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
     aeson base base16-bytestring binary bytestring cryptonite hashable
-    http-types iso8601-time lucid monad-control mtl
+    htoml http-types iso8601-time lucid monad-control mtl
     optparse-applicative postgresql-simple process resource-pool scotty
     text time unordered-containers uuid wai wai-extra
     wai-middleware-static
   ];
   license = stdenv.lib.licenses.unfree;
+  hydraPlatforms = stdenv.lib.platforms.none;
 }
--- shell.nix
+++ shell.nix
@@ -1,36 +1,40 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
 
 let
 
   inherit (nixpkgs) pkgs;
 
   f = { mkDerivation, aeson, base, base16-bytestring, binary
-      , bytestring, cryptonite, hashable, http-types, iso8601-time, lucid
-      , monad-control, mtl, optparse-applicative, postgresql-simple
-      , process, resource-pool, scotty, stdenv, text, time
-      , unordered-containers, uuid, wai, wai-extra, wai-middleware-static
+      , bytestring, cryptonite, hashable, htoml, http-types, iso8601-time
+      , lucid, monad-control, mtl, optparse-applicative
+      , postgresql-simple, process, resource-pool, scotty, stdenv, text
+      , time, unordered-containers, uuid, wai, wai-extra
+      , wai-middleware-static
       }:
       mkDerivation {
         pname = "lodjur";
-        version = "0.0.0";
+        version = "0.1.0";
         src = ./.;
         isLibrary = false;
         isExecutable = true;
         executableHaskellDepends = [
           aeson base base16-bytestring binary bytestring cryptonite hashable
-          http-types iso8601-time lucid monad-control mtl
+          htoml http-types iso8601-time lucid monad-control mtl
           optparse-applicative postgresql-simple process resource-pool scotty
           text time unordered-containers uuid wai wai-extra
           wai-middleware-static
         ];
         license = stdenv.lib.licenses.unfree;
+        hydraPlatforms = stdenv.lib.platforms.none;
       };
 
   haskellPackages = if compiler == "default"
                        then pkgs.haskellPackages
                        else pkgs.haskell.packages.${compiler};
 
-  drv = haskellPackages.callPackage f {};
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
 
 in
 
