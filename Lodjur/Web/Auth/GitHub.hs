7937174a7bc0371030bb6062b8a729d7550d2d74
Shaun Sharples <shaun.sharples@gmail.com>
2018-10-04 14:38:57 +0200

    Get User and Teams from GitHub before allowing login

--- Lodjur/Web/Auth/GitHub.hs
+++ Lodjur/Web/Auth/GitHub.hs
@@ -2,9 +2,14 @@
 {-# LANGUAGE RecordWildCards   #-}
 module Lodjur.Web.Auth.GitHub where
 
-import           Control.Monad.IO.Class
-import qualified Data.Text                 as Text
-import qualified Data.Text.Encoding        as Text
+import           Control.Monad.Except
+import           Control.Monad.IO.Class               (liftIO)
+import           Data.Foldable                        (toList)
+import qualified Data.Text                            as Text
+import qualified Data.Text.Encoding                   as Text
+import qualified GitHub
+import qualified GitHub.Endpoints.Organizations.Teams as GitHub
+import qualified GitHub.Endpoints.Users               as GitHub
 import           Network.HTTP.Client
 import           Network.HTTP.Client.TLS
 import           Network.HTTP.Types.Status
@@ -26,23 +31,64 @@ startGithubAuthentication oauth2 = do
 
 exchangeCode :: OAuth2 -> Action ()
 exchangeCode oauth2 = do
-  code   <- param' "code"
-  result <- liftIO $ do
-    manager <- newManager tlsManagerSettings
-    fetchAccessToken manager oauth2 (ExchangeToken code)
-  case result of
-    Left err -> do
+  merr   <- param "error"
+  case merr of
+    Just err -> do
       setStatus status400
-      writeSession Session { currentUser = Nothing, continueTo = Nothing }
-      text (Text.pack (show err))
-    Right OAuth2Token {..} -> do
-      -- TODO: Request this information from GitHub API
-      let user = User { userId = UserId "johndoe" }
-      continueTo' <- continueTo <$> readSession
-      writeSession Session { currentUser = Just user, continueTo = Nothing }
-      case continueTo' of
-        Just relRef -> redirect (Text.decodeUtf8 (serializeURIRef' relRef))
-        Nothing -> redirect "/"
+      allParams <- params
+      text ("Error: " <> err <> ", Params: " <> Text.pack (show allParams))
+    Nothing -> do
+      code   <- param' "code"
+      result <- liftIO $ do
+        manager <- newManager tlsManagerSettings
+        fetchAccessToken manager oauth2 (ExchangeToken code)
+      case result of
+        Left err -> do
+          setStatus status400
+          writeSession Session { currentUser = Nothing, continueTo = Nothing }
+          text (Text.pack (show err))
+        Right OAuth2Token {..} -> do
+          githubResult <- liftIO . runExceptT $ do
+            user <- ExceptT (getUser accessToken)
+            teams <- ExceptT (getTeams accessToken)
+            return (user, teams)
+          case githubResult of
+            Left err -> do
+              setStatus status400
+              text ("Failed: " <> Text.pack (show err))
+            Right (githubUser, teams)
+              | isAuthorized teams -> do
+                let user = User { userId = UserId (GitHub.untagName $ GitHub.userLogin githubUser) }
+                continueTo' <- continueTo <$> readSession
+                writeSession Session { currentUser = Just user, continueTo = Nothing }
+                case continueTo' of
+                  Just relRef -> redirect (Text.decodeUtf8 (serializeURIRef' relRef))
+                  Nothing -> redirect "/"
+              | otherwise -> do
+                setStatus status403
+                text "You are not authorized, because you don't have read/write permissions in the mpowered/core team."
+
+isAuthorized :: [GitHub.Team] -> Bool
+isAuthorized = any authorizedInTeam
+  where
+    authorizedInTeam t = team t == "core" && org t == "mpowered"
+    team = GitHub.untagName . GitHub.teamSlug
+    org  = GitHub.untagName . GitHub.simpleOrganizationLogin . GitHub.teamOrganization
+
+
+toOAuth :: AccessToken -> GitHub.Auth
+toOAuth (AccessToken accessToken) =
+  GitHub.OAuth (Text.encodeUtf8 accessToken)
+
+getUser :: AccessToken -> IO (Either GitHub.Error GitHub.User)
+getUser at =
+  -- TODO use manager?
+  GitHub.userInfoCurrent' (toOAuth at)
+
+getTeams :: AccessToken -> IO (Either GitHub.Error [GitHub.Team])
+getTeams at =
+  -- TODO use manager?
+  fmap toList <$> GitHub.listTeamsCurrent' (toOAuth at)
 
 clearSession :: Action ()
 clearSession = do
