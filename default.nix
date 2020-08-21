568bb659f1434901f11e754646d500523d811b38
Shaun Sharples <shaun.sharples@gmail.com>
2020-08-21 16:47:52 +0200

    trying to make with haskell.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,30 @@
 let
-  drv = (import ./lodjur.nix).lodjur;
+  sources = import ./nix/sources.nix;
 in
-  drv
+{ # Fetch the latest haskell.nix and import its default.nix
+  haskellNix ? import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz") {}
+
+# haskell.nix provides access to the nixpkgs pins which are used by our CI,
+# hence you will be more likely to get cache hits when using these.
+# But you can also just use your own, e.g. '<nixpkgs>'.
+, nixpkgsSrc ? haskellNix.sources.nixpkgs-2003
+
+# haskell.nix provides some arguments to be passed to nixpkgs, including some
+# patches and also the haskell.nix functionality itself as an overlay.
+, nixpkgsArgs ? haskellNix.nixpkgsArgs
+
+# import nixpkgs with overlays
+, pkgs ? import nixpkgsSrc (nixpkgsArgs // {
+    overlays = nixpkgsArgs.overlays ++ [
+      # (import sources.all-hies {}).overlay
+    ];
+  })
+}: pkgs.haskell-nix.project {
+  # 'cleanGit' cleans a source directory based on the files known by git
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "lodjur";
+    src = ./.;
+  };
+  # For `cabal.project` based projects specify the GHC version to use.
+  compiler-nix-name = "ghc865"; # Not used for `stack.yaml` based projects.
+}
--- shell.nix
+++ shell.nix
@@ -1,5 +1,29 @@
+{ }:
+
 let
-  shell = (import ./lodjur.nix).lodjur-shell-with;
-  devpkgs = p: [ p.hlint ]; # p.brittany ];
+  hsPkgs = import ./default.nix { };
 in
-  shell devpkgs
+  hsPkgs.shellFor {
+    # Include only the *local* packages of your project.
+    packages = ps: with ps; [
+      lodjur
+    ];
+
+    # Builds a Hoogle documentation index of all dependencies,
+    # and provides a "hoogle" command to search the index.
+    withHoogle = true;
+
+    # You might want some extra tools in the shell (optional).
+
+    # Some common tools can be added with the `tools` argument
+    tools = { cabal = "3.2.0.0"; hlint = "2.2.11"; ghcide = "0.2.0"; }; # hie = "unstable";
+    # See overlays/tools.nix for more details
+
+    # Some you may need to get some other way.
+    # buildInputs = with pkgs.haskellPackages;
+      # [ ghcid ];
+
+    # Prevents cabal from choosing alternate plans, so that
+    # *all* dependencies are provided by Nix.
+    exactDeps = true;
+  }
